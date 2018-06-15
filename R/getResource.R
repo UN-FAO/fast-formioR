3c7632d381db43d14aef0b66a573bfae58d25f28
Ignacio Cabrera <cabrerabywaters@gmail.com>
2018-06-15 12:02:01 +0200

    docs

--- R/getResource.R
+++ R/getResource.R
@@ -7,22 +7,28 @@
 #' @keywords formio
 #' @export
 #' @examples
-#' FormioInterface()
-FormioInterface <-  R6::R6Class("FormioInterface",   
+#' Formior()
+Formior <-  R6::R6Class("Formior",   
     public = list(
+    # Class properties
     base_url = NULL,
     resource_path = NULL,
     token = NULL,
     filter_string = NULL,
     select_string = NULL,
     limit_string = NULL,
+    populate_string = NULL,
     chunk_size_string = NULL,
     first_chunk = NULL,
+    
+    # Class constructor
     initialize = function(base_url = NA, resource_path = NA, token = NA) {
       self$base_url <- base_url
       self$resource_path <- resource_path
       self$token <- token
     },
+    
+    #Methods
     
     #' Sets the proper Filter URL for the GET CALL
     #'
@@ -56,19 +62,17 @@ FormioInterface <-  R6::R6Class("FormioInterface",
         else if(query == "<="){
           filterString <- paste(filterString, element, '__lte=', value, '&', sep ='')
         }
-        # Implementation pending
         else if(query == "in"){
-          filterString <- paste(filterString, element, '__ne=', value, '&', sep ='')
+          filterString <- paste(filterString, element, '__in=', value, '&', sep ='')
         }
-        # Implementation pending
         else if(query == "nin"){
-          filterString <- paste(filterString, element, '__ne=', value, '&', sep ='')
+          filterString <- paste(filterString, element, '__nin=', value, '&', sep ='')
         }
         else if(query == "exists"){
           filterString <- paste(filterString, element, '__exists=', TRUE, '&', sep ='')
         }
         else if(query == "!exists"){
-          filterString <- paste(filterString, element, '__ne=', FALSE, '&', sep ='')
+          filterString <- paste(filterString, element, '__exists=', FALSE, '&', sep ='')
         }
         else if(query == "regex"){
           filterString <- paste(filterString, element, '__regex=', value, '&', sep ='')
@@ -86,28 +90,61 @@ FormioInterface <-  R6::R6Class("FormioInterface",
     #' @examples
     #' selectToString()
     selectToString = function(select){
-      selectString <- '_id,owner,modified,'
+      selectString <- '_id'
       for(attribute in select){
         selectString = paste(selectString,attribute,',',sep='')
       }
       selectString <- substr(selectString, 1, nchar(selectString)-1) 
       return(selectString)
     },
+    #' Sets the proper Select URL for the GET CALL
+    #'
+    #' Parses the list of select elements to a single string
+    #' @param filter The list of items to include in the select
+    #' @keywords filter
+    #' @examples
+    #' selectToString()
+    populateToString = function(resources){
+      populateString <- ''
+      for(resource in resources){
+        populateString = paste(populateString,resource,',',sep='')
+      }
+      populateString <- substr(populateString, 1, nchar(populateString)-1) 
+      return(populateString)
+    },
+    
+    #' Gets the first Query String
+    #'
+    #' Returns the first query string given all the filters, select and limit
+    #' @examples
+    #' getChunkSize()
     getQueryString = function(){
       
       url <- paste(self$base_url, self$resource_path, "/submission?", sep='')
       first_chunk <- NULL
       
-      # Set the final queryString
+      # Sets the final queryString
+      # Adds the filterString
       if(is.character(self$filter_string)) {
         url <- paste(url, self$filter_string, '&', sep="")
       } 
+      # Adds the selectString
       if(is.character(self$select_string)) {
         url <- paste(url,'select=', self$select_string, '&', sep="")
+      } 
+      # Adds the populateString
+      if(is.character(self$populate_string)) {
+        url <- paste(url,'populate=', self$populate_string, '&', sep="")
       } 
       return(url)
       
     },
+    
+    #' Gets the first chunk size
+    #'
+    #' Returns the first chunk size depending on the limit and chunks defined
+    #' @examples
+    #' getChunkSize()
     getChunkSize = function(){
       first_chunk <- NULL
       url <- ''
@@ -131,6 +168,13 @@ FormioInterface <-  R6::R6Class("FormioInterface",
       self$first_chunk <- first_chunk
       return(url)
     },
+    getTokenType = function(){
+      if(nchar(self$token) > 32) {
+        return('x-jwt-token')
+      }
+      return('x-token')
+    },
+
     #' Extract all results of a given resource or form
     #'
     #' This function allows you to express your love of cats.
@@ -143,8 +187,15 @@ FormioInterface <-  R6::R6Class("FormioInterface",
       url <- paste(self$getQueryString(), self$getChunkSize(), sep="")
       url <- substr(url, 1, nchar(url)-1) 
       last_iteration_limit <- NULL
+      tokenType = self$getTokenType()
+     
       # GET and Parse the content
-      firstResult <- httr::GET(url, httr::add_headers("x-token" = self$token), httr::accept_json())
+      if(tokenType == 'x-token') {
+        firstResult <- httr::GET(url, httr::add_headers('x-token' = self$token), httr::accept_json())
+      } else {
+        firstResult <- httr::GET(url, httr::add_headers('x-jwt-token' = self$token), httr::accept_json())
+      }
+      
       
       error <- httr::http_error(firstResult)
       if(error){
@@ -204,7 +255,12 @@ FormioInterface <-  R6::R6Class("FormioInterface",
         }
         it_url <- paste(self$getQueryString(), "&limit=", limit, "&skip=", skip_amount, sep=""); 
         # GET and Parse the content
-        more_result <- httr::GET(it_url, httr::add_headers("x-token" = self$token), httr::accept_json())
+        if(tokenType == 'x-token') {
+          more_result <- httr::GET(it_url, httr::add_headers("x-token" = self$token), httr::accept_json())
+        } else {
+          more_result <- httr::GET(it_url, httr::add_headers("x-jwt-token" = self$token), httr::accept_json())
+        }
+        
         more_content <- httr::content(more_result, as = "text", encoding = "UTF-8")
         more_json <- jsonlite::fromJSON(more_content, flatten = TRUE)
         
@@ -220,6 +276,7 @@ FormioInterface <-  R6::R6Class("FormioInterface",
       final_result <- jsonlite::rbind_pages(chunks)
       return(final_result)
     },
+    
     #' Applies the filters
     #'
     #' @param filters the list of filters
@@ -230,6 +287,7 @@ FormioInterface <-  R6::R6Class("FormioInterface",
       self$filter_string = self$filterToString(filters)
       return(self)
     },
+    
     #' Applies the select
     #'
     #' @param select the list of select
@@ -240,6 +298,7 @@ FormioInterface <-  R6::R6Class("FormioInterface",
       self$select_string = self$selectToString(select)
       return(self)
     },
+    
     #' Applies the limit to the Query
     #'
     #' @param limit the limit for the query
@@ -250,13 +309,25 @@ FormioInterface <-  R6::R6Class("FormioInterface",
       self$limit_string = as.character(limit)
       return(self)
     },
+    
+    #' Applies the populate to the Query
+    #'
+    #' @param resources the populate resources for the query
+    #' @export
+    #' @examples
+    #' limit()
+    populate = function(resources){
+      self$populate_string = self$populateToString(resources)
+      return(self)
+    },
+    
     #' Defines how many elements to call each time
     #'
     #' @param size Number of elements on each call
     #' @export
     #' @examples
     #' limit()
-    chunkSize = function(size){
+    chunks = function(size){
       self$chunk_size_string = as.character(size)
       return(self)
     }
---
+++ README.md
@@ -0,0 +1,109 @@
+# FAST - formioR
+
+A small R library to ease your work with R.
+A wraper Class on top of the Form.io API
+
+### Installing
+
+To install this package in your R project, you will need to have the 'devtools' package (soon we will move it to CRAN)
+
+```
+install.packages("devtools")
+library("devtools")
+```
+
+Then you can install this directly from Github using
+
+```
+install_github('fast-formioR','UN-FAO')
+```
+
+# Usage
+
+Until now we only support the GET HTTP request, as we mainly use this library to pull data for statistics, the package works in the following way
+
+# Quick Example
+
+No time to read? No worries, here you have a quick example of all of the think you can do
+
+```R
+token <- "P9reVWfdJiI5rc7qre4bn2HVcdyJJi1"
+url <- "https://myAPI.form.io/"
+User <- formior::Formior$new(url, "user", token)
+filter <- list(list('_id','!=','5a65acec16987c0001f3d246'), list('owner','!=','2342342344'))
+select <- list('data.a', 'owner', '_id')
+limit <- 150
+chunk_size <- 15
+populate <- list('owner')
+
+# Apply the filter, limit
+# All methods except get() return the self object (this)
+User$select(select)$filter(filter)$limit(limit)$populate(populate)$chunks(chunk_size)
+
+# If you want you can also do it the "R" way and be a little more verbose about it
+User$select(select)
+User$filter(filter)
+User$limit(limit)
+User$populate(populate)
+User$chunks(chunk_size)
+
+#Once you are ready, just pull your results with get()
+data <- User$get()
+```
+
+## The FormioR instance
+
+```R
+User <- Formior$new(
+			base_url,      #Base URL of your Form.io project
+			resource_name, #The API path of the resource or Form that you want to use
+			token          # token or jwt-token for Auth
+		)
+```
+
+Once you have your instance you can go and Query the resource by using chained methods. The available methods are the following
+
+## Methods
+
+| Method   | Parameter Type           | Description                                                      |
+| -------- | ------------------------ | ---------------------------------------------------------------- |
+| select   | list()                   | List of all the attributes you want to pull                      |
+| filter   | list(list(), list(),...) | A list of all filter queries (See filter section)                |
+| limit    | numeric                  | The max amount of elements to pull                               |
+| populate | list()                   | List of all linked resources to populate                         |
+| chunks   | numeric                  | How many elements to pull at the time (usefull for big datasets) |
+| get      | NONE                     | Applies all methods and generates the query                      |
+
+## Filter method
+
+The filter method receives a list with filters inside. Every filter is also a list defined in the following way:
+
+```R
+filter <- list(
+	list(
+		'attribute',       #The attribute of our Resource or Form to filter
+		'operator/query',  #The operator i.e= '=' , '!=' (See list of Queries)
+		'value'            #The operator will check the attribute against this value
+	)
+)
+```
+
+You can have as many filters as you want, but know that all filters will follow the "and" operator (&&)
+so if you apply two filters they will work as: Filter1 && Filter2. The or operator is not yet supported
+for multiple filters
+
+## Operators
+
+| Operator                 | Query   | Example                                   |
+| ------------------------ | ------- | ----------------------------------------- |
+| Equal                    | =       | list('data.gender', '=', 'male')          |
+| Not equal                | !=      | list('data.gender', '!=', 'male')         |
+| Greater Tthan            | >       | list('data.age', '>', '18')               |
+| Greater than or equal to | >=      | list('data.age', '>=', '18')              |
+| Less than                | <       | list('data.age', '<', '30')               |
+| Less than or equal to    | <=      | list('data.age', '<=', '30')              |
+| In                       | in      | list('data.gender', 'in', 'male,female')  |
+| Not in                   | nin     | list('data.age', 'nin', '18,21')          |
+| Exists                   | exists  | list('data.age', 'exists')                |
+| Not exists               | !exists | list('data.age', '!exists')               |
+| Regex                    | regex   | list('data.username', 'regex', '/^cab/i') |
